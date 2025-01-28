8f62b1578c94d170f60677b7ae050924aa37bd81
Oltthaqi <oltthaqi@gmail.com>
2025-01-28 20:52:51 +0100

    Added Cors config

--- src/main/java/dev/babat/sems/schoolsystem0managementsems/SchoolSystem0managementSemsApplication.java
+++ src/main/java/dev/babat/sems/schoolsystem0managementsems/SchoolSystem0managementSemsApplication.java
@@ -2,6 +2,8 @@ package dev.babat.sems.schoolsystem0managementsems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SchoolSystem0managementSemsApplication {
@@ -10,4 +12,17 @@ public class SchoolSystem0managementSemsApplication {
 		SpringApplication.run(SchoolSystem0managementSemsApplication.class, args);
 	}
 
+
+	@Bean
+	public WebMvcConfigurer webMvcConfigurer() {
+		return new WebMvcConfigurer() {
+		@Override
+			public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:3000")
+						.allowedMethods("GET", "POST", "PUT", "DELETE")
+						.allowedHeaders("*");
+			}
+		};
+	}
 }
